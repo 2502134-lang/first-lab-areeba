378640794b41c351d15fcf5a37af647e26b2c3fe
2502134-lang <2502134@students.au.edu.pk>
2026-02-25 20:52:35 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+
+import { add, subtract } from './math-utils.js';
+// add function use
+const sumResult = add(10, 5);
+//subtract function use
+const diffResult = subtract(20, 7);
+//Results 
+console.log("Addition Result (10 + 5):", sumResult);
+console.log("Subtraction Result (20 - 7):", diffResult);
---
+++ math-utils.js
@@ -0,0 +1,7 @@
+//module
+export const add = (a, b) => {
+    return a + b;
+};
+export const subtract = (a, b) => {
+    return a - b;
+};
